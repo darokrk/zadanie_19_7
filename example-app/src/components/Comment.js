e22a10a4a1b7315949a76f2a69bb1280d84a7ef8
Dariusz Rosiek <daro.krk@interia.pl>
2018-04-13 23:48:24 +0200

    Add thumbUpComment button and editComment

--- example-app/src/components/Comment.js
+++ example-app/src/components/Comment.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
-const Comment = ({text, votes, id, thumbUpComment}) =>
+const Comment = ({text, votes, id, thumbUpComment, thumbDownComment, editComment}) =>
     <li>
         {text} <span>votes: {votes} </span>
         <button onClick={() => thumbUpComment(id)}>
             Thumb up
+        </button>
+        <button onClick={() => thumbDownComment(id)}>
+            Thumb down
+        </button>
+        <button onClick={() => {
+            var text = prompt('Edit comment');
+            editComment(text, id)
+        }}>
+            Edit
         </button>
     </li>;
 
--- example-app/src/containers/CommentContainer.js
+++ example-app/src/containers/CommentContainer.js
@@ -1,9 +1,15 @@
 import {connect} from 'react-redux';
 import Comment from '../components/Comment';
-import {thumbUpComment} from '../actions/actions.js';
+import {
+    thumbUpComment,
+    thumbDownComment,
+    editComment
+} from '../actions/actions.js';
 
 const mapDispatchToProps = dispatch => ({
-    thumbUpComment: (id) => dispatch(thumbUpComment(id))
+    thumbUpComment: (id) => dispatch(thumbUpComment(id)),
+    thumbDownComment: (id) => dispatch(thumbDownComment(id)),
+    editComment: (text, id) => dispatch(editComment(text, id))
 });
 
 export default connect(null, mapDispatchToProps)(Comment);
